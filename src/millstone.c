bd6f6de4195b30ffabb52ac77261b52943f4531f
Herman Narkaytis <hnarkaytis@gmail.com>
2013-09-20 19:05:32 +0400

    Fixed cores counting by hyper-threading detection.

--- src/millstone.c
+++ src/millstone.c
@@ -38,23 +38,10 @@ get_cores ()
 {
   int cores = (long) sysconf (_SC_NPROCESSORS_ONLN);
   unsigned int regs[4];
-  char vendor[sizeof (regs) + 1];
 
-  memset (vendor, 0, sizeof (vendor));
-  cpu_id (0, (void*)vendor); /* Get vendor */
-  if (strcmp (vendor, "GenuineIntel") == 0)
-    {
-      /* Get DCP cache info */
-      cpu_id (4, regs);
-      cores = ((regs[0] >> 26) & 0x3f) + 1; /* EAX[31:26] + 1 */
-    }
-  else if (strcmp (vendor, "AuthenticAMD") == 0)
-    {
-      /* Get NC: Number of CPU cores - 1 */
-      cpu_id (0x80000008, regs);
-      cores = ((unsigned)(regs[2] & 0xff)) + 1; /* ECX[7:0] + 1 */
-    }
-  
+  cpu_id (1, regs); /* Get CPU features */
+  if (regs[3] & (1 << 28)) /* EDX:28 - hyper-threading */
+    cores >>= 1;
   return (cores);
 }
 
@@ -123,6 +110,7 @@ parse_args (int argc, char * argv[], config_t * config)
 #endif /* HAVE_ZLIB */
   if (config->workers_number <= 0)
     config->workers_number = 1;
+  INFO_MSG ("Workers %d.", config->workers_number);
   
   if (argc - optind == 0)
     config->run_mode = RM_SERVER;
